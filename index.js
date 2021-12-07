e9a23a6ff7ee91e2fe9b2b86dc82e5c8c710cc4f
Mariusz Walusiak <marwal91@o2.pl>
2021-12-07 16:15:04 +0100

    task1

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const cookieButton = document.querySelector("#make-cookie");
 const progressBar = document.querySelector(".progress-bar");
+const counterBakedCookies = document.querySelector('#counter')
+
 
 var duration;
 let acceleration = false;
+progressBar.style.width = "0px";
+console.log(duration);
+let bakedCookies = 0;
 
-function intervalFunction() {
-  progressBar.style.width = "0px";
+//function declaration
+function startInterval() {
   duration = setInterval(runProgressBar, 1000);
+  acceleration = true;
+  cookieButton.textContent = "zatrzymaj lepienie";
+  console.log(duration);
 }
 
 function stopInterval() {
   clearInterval(duration);
+  acceleration = false;
+  cookieButton.textContent = "ulep ciasto";
+  console.log(duration);
 }
 
 function runProgressBar() {
-  acceleration = true;
   if (progressBar.style.width === "0px") {
     progressBar.style.width = "25px";
   } else if (progressBar.style.width === "25px") {
@@ -23,6 +33,7 @@ function runProgressBar() {
     progressBar.style.width = "75px";
   } else if (progressBar.style.width === "75px") {
     progressBar.style.width = "100px";
+    countBakedCookies();
   } else if (progressBar.style.width === "100px") {
     progressBar.style.width = "0px";
   }
@@ -30,10 +41,17 @@ function runProgressBar() {
   console.log(progressBar.style.width);
 }
 
+function countBakedCookies(){
+    bakedCookies++;
+    counterBakedCookies.textContent = `Liczba ulepionych ciastowych kul: ${bakedCookies}`
+    return bakedCookies;
+}
+
+//button init
 cookieButton.addEventListener("click", function () {
-  if (acceleration === true) {
-    stopInterval();
+  if (acceleration === false) {
+    startInterval();
   } else {
-    intervalFunction();
+    stopInterval();
   }
 });
