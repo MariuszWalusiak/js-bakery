05965e4386f9877a28179e8de4030f156048b200
Mariusz Walusiak <marwal91@o2.pl>
2021-12-07 15:03:08 +0100

    wip-1.c

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const cookieButton = document.querySelector("#make-cookie");
 const progressBar = document.querySelector(".progress-bar");
 
+var duration;
+let acceleration = false;
+
+function intervalFunction() {
+  progressBar.style.width = "0px";
+  duration = setInterval(runProgressBar, 1000);
+}
+
+function stopInterval() {
+  clearInterval(duration);
+}
+
+function runProgressBar() {
+  acceleration = true;
+  if (progressBar.style.width === "0px") {
+    progressBar.style.width = "25px";
+  } else if (progressBar.style.width === "25px") {
+    progressBar.style.width = "50px";
+  } else if (progressBar.style.width === "50px") {
+    progressBar.style.width = "75px";
+  } else if (progressBar.style.width === "75px") {
+    progressBar.style.width = "100px";
+  } else if (progressBar.style.width === "100px") {
+    progressBar.style.width = "0px";
+  }
+
+  console.log(progressBar.style.width);
+}
 
 cookieButton.addEventListener("click", function () {
-  progressBar.classList.add("progress-bar-animation");
+  if (acceleration === true) {
+    stopInterval();
+  } else {
+    intervalFunction();
+  }
 });
